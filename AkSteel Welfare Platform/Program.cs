cb6ef678dbeb0105dd6eb68eeb8b4a23fa10bf78
AdminHomePc <AdminHomePC@DESKTOP-BSIK1TP>
2025-10-09 11:42:00 +0330

    create aksteel welfare platform

---
+++ AkSteel Welfare Platform/Program.cs
@@ -0,0 +1,56 @@
+using Auth.Infrastructure.DependencyInjection;
+using Auth.Presentation.DependencyInjection;
+using Auth.Application.DependencyInjection;
+using Core.Infrastructure.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// configuration
+var configuration = builder.Configuration;
+
+builder.Services.AddCoreInfrastructure();
+builder.Services.AddAuthApplication();
+builder.Services.AddAuthInfrastructure(configuration);
+
+builder.Services.AddAuthPresentation();
+
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+
+var allowedOrigins = configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AppCorsPolicy", policy =>
+    {
+        policy.WithOrigins(allowedOrigins ?? Array.Empty<string>())
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseCors("AppCorsPolicy");
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.Services.SeedAuthModuleAsync();
+app.Run();
