1c37538133dfa43c6f6ec3842d3c17654b508d93
868 <868@aksteel>
2025-10-28 10:35:33 +0330

    .

--- Export-Modules.ps1
+++ Export-Modules.ps1
@@ -1,17 +1,28 @@
-﻿$root = ""   # مسیر ریشه پروژه‌ات
-$output = "exports"  # جایی که خروجی‌ها ذخیره میشه
+﻿# راهنمای اجرا 
+# ایتدا در powerShell به محل فایل برویم
+# cd C:\Users\Mahar\source\repos\csharperBoy\NexusPlatform
+# داخل PowerShell دستور زیر رو با پارامتر های صحیح اجرا کنید
+#.\Export-Modules.ps1 -SolutionPath "C:\Users\Mahar\source\repos\csharperBoy\NexusPlatform" -OutputPath "C:\Users\Mahar\source\repos\csharperBoy\NexusPlatform\TreeOutput.txt"
+# اگر خطای دسترسی داد با دستور زیر دسترسی رو اوکی کن بعد اجرا کن
+#Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass # موقت
+#Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned # دائم
+
+param(
+    [string]$Root = "src",        # مسیر ریشه پروژه
+    [string]$Output = "exports"   # مسیر خروجی
+)
 
 # اگر پوشه خروجی وجود نداشت بساز
-if (!(Test-Path $output)) {
-    New-Item -ItemType Directory -Path $output | Out-Null
+if (!(Test-Path $Output)) {
+    New-Item -ItemType Directory -Path $Output | Out-Null
 }
 
-# برای هر ماژول سطح اول
-Get-ChildItem -Path $root -Directory | ForEach-Object {
-    $moduleName = $_.Name
-    $outFile = Join-Path $output "$moduleName.txt"
+# برای هر ماژول سطح اول (Auth, UserManagement, Caching, ...)
+Get-ChildItem -Path $Root -Directory | ForEach-Object {
+    $moduleName = $_.Name.Split('.')[0]   # فقط بخش اول اسم (Auth از Auth.Application)
+    $outFile = Join-Path $Output "$moduleName.cs"
 
-    # همه فایل‌های cs داخل اون ماژول
+    # همه فایل‌های cs داخل همه‌ی زیرپروژه‌های این ماژول
     Get-ChildItem -Path $_.FullName -Recurse -Filter *.cs |
         ForEach-Object {
             "//// FILE: $($_.FullName)" | Out-File $outFile -Append -Encoding utf8
