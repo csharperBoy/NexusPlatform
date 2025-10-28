aabc29886da77e8b8f3683eaac323862350f6e65
868 <868@aksteel>
2025-10-28 11:17:50 +0330

    .

---
+++ User.Infrastructure/DependencyInjection/HealthCheckExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using User.Infrastructure.Data;
+
+
+namespace User.Infrastructure.DependencyInjection
+{
+    public static class HealthCheckExtensions
+    {
+        public static IServiceCollection Audit_AddHealthChecks(this IServiceCollection services, IConfiguration config)
+        {
+            var conn = config.GetConnectionString("DefaultConnection");
+
+            services.AddHealthChecks()
+                    .AddDbContextCheck<UserManagementDbContext>("UserDatabase");
+
+
+            return services;
+        }
+    }
+}
