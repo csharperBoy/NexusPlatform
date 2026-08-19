cc3acb3685f4e10dee9add302c906d173b6bc63e
868 <868@WINFDEV1>
2026-08-19 21:30:44 +0330

    migrations hr contact people

---
+++ People.Infrastructure/Migrations/20260819175938_Edit_3_People.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace People.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Edit_3_People : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "FkContactProfileId",
+                schema: "people",
+                table: "Parties",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FkContactProfileId",
+                schema: "people",
+                table: "Parties");
+        }
+    }
+}
